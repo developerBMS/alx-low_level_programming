b273b74b9725d5cf1cab84da52f736da83df7cc4
developerBMS <sekhulabm@gmail.com>
2023-11-16 06:54:46 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string in reverse
+ *@s: first value to check
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = (i - 1); j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ *@a: first value to check
+ *@n: Second value to check
+ *
+ */
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n;  i++)
+	{
+		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
